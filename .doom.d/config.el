29fa0bb79a53ae7c788bdebcef45440058e12de3
Adam Velebil <pardalek@gmail.com>
2023-11-29 07:45:10 +0100

    [doom] improve python support

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -90,3 +90,12 @@
 (setq-hook! 'c++-mode-hook +format-with-lsp nil)
 
 (setq confirm-kill-emacs nil)
+
+(use-package! python-black
+  :demand t
+  :after python)
+(add-hook! 'python-mode-hook #'python-black-on-save-mode)
+;; Feel free to throw your own personal keybindings here
+(map! :leader :desc "Blacken Buffer" "m b b" #'python-black-buffer)
+(map! :leader :desc "Blacken Region" "m b r" #'python-black-region)
+(map! :leader :desc "Blacken Statement" "m b s" #'python-black-statement)
